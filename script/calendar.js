1ad78bf4ab1f38fb6eecff818b27506e6c6e8e78
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2020-12-01 11:36:56 +0100

    Hamburger menu fixed on firefox and adding 0 in case of date like 2020-12-01

--- script/calendar.js
+++ script/calendar.js
@@ -5,6 +5,12 @@ function calendar(){
 	var day=date.getDate();
 	var month=date.getMonth()+1;
 	var year=date.getYear()+1900;
+	if (day<10){
+		day="0"+day;
+	}
+	if (month<10){
+		month="0"+month;
+	}
 	$("#datePicker").attr('value',year+"-"+month+"-"+day);
 						 
 	$("#datePicker").datepicker({
